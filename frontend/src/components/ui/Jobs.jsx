8500eddd6d6ae84f8d4af5120bac03f3025e8c03
anshu-sharma603 <anshusharma8981@gmail.com>
2026-08-31 20:49:41 +0530

    Update Jobs component

--- frontend/src/components/ui/Jobs.jsx
+++ frontend/src/components/ui/Jobs.jsx
@@ -3,15 +3,18 @@ import Navbar from './shared/Navbar'
 import FilterCard from './FilterCard'
 import Job from './Job';
 
-const jobArray = [1,2,3,4,5,6,7,8];
+const jobArray = [1, 2, 3, 4, 5, 6, 7, 8];
 function Jobs() {
   return (
     <div>
-        <Navbar/>
-        <FilterCard/>
+      <Navbar />
+      <div className='max-w-7xl msx-auto mt-5'>
+        <FilterCard />
         {
-            jobArray.map((element,index)=> <Job/>)
+          jobArray.map((element, index) => <Job />)
         }
+      </div>
+
     </div>
   )
 }
